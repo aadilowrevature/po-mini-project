f98e9489ed585d97357902c4db8199c19a82511f
Gorm <gdsoucie@gmail.com>
2021-08-11 16:09:12 -0400

    grouped articles into article objects

--- rss-feed-reader/src/app/reader-test/reader-test.component.ts
+++ rss-feed-reader/src/app/reader-test/reader-test.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Article} from '../Article';
 
 
 let rss_url: string = 'https://www.usda.gov/rss/home.xml';
@@ -15,6 +16,7 @@ export class ReaderTestComponent implements OnInit {
 
    CORS_PROXY = "https://corsanywhere.herokuapp.com/"
   
+   articles:Article[] = [];
 
    title:string[]=[];
    description:string[]=[];
@@ -31,11 +33,20 @@ export class ReaderTestComponent implements OnInit {
         console.log(data.documentElement.getElementsByTagName("title").length)
 
         for (let i = 0; i < data.documentElement.getElementsByTagName("title").length; i++) {
-           
+          
         this.title[i]=data.documentElement.getElementsByTagName("title")[i].innerHTML;
         this.description[i]=data.documentElement.getElementsByTagName("description")[i].innerHTML;
-        this.link[i]=data.documentElement.getElementsByTagName("link")[i].innerHTML;;
+        this.link[i]=data.documentElement.getElementsByTagName("link")[i].innerHTML;
+        let art:Article = {
+          'title': data.documentElement.getElementsByTagName("title")[i].innerHTML,
+          'description': data.documentElement.getElementsByTagName("description")[i].innerHTML,
+          'link': data.documentElement.getElementsByTagName("link")[i].innerHTML
         }
+        //may want to strip broken http from this...
+        this.articles.push(art);
+        // console.log("target")
+        // console.log(data.documentElement.getElementsByTagName("description")[3].textContent)
+      }
        
         console.log(this.title)
         console.log(this.description)
