06a9558917bc899526e59d58e5a5970cb4d3a23d
Gorm <gdsoucie@gmail.com>
2021-08-11 15:27:03 -0400

    gorm played with his test component

---
+++ rss-server/src/test/java/com/gorm/rssserver/RssServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gorm.rssserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RssServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
