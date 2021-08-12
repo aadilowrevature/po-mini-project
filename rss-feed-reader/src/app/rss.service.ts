e5a966c0e87e1ef3f4e12b3752c49a8d0d773548
Gorm <gdsoucie@gmail.com>
2021-08-12 11:45:17 -0400

    added if statement to rssService so that some errors are handled

--- rss-feed-reader/src/app/rss.service.ts
+++ rss-feed-reader/src/app/rss.service.ts
@@ -11,6 +11,8 @@ export class RssService {
 
   articles: Article[] = [];
 
+  urlList: string[] = []; //holds list of urls
+
   articleEmitter = new Subject<Article[]>();
   //switch to behavior subject...
   //new EventEmitter<Article[]>(); //produced event...
@@ -19,33 +21,46 @@ export class RssService {
 
   constructor() { }
 
-  setData(rss_url: string):Article[] {
+  setData(rss_url: string): Article[] {
     fetch(this.CORS_PROXY + rss_url
     ).then(res => res.text())
       .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
       .then(data => {
         console.log(data)
         console.log(data.documentElement.getElementsByTagName("title").length)
-//empty data so we dont just add to it...
-        this.articles = [];
+        //empty data so we dont just add to it...
+        this.articles = []; // deletes old list
         for (let i = 0; i < data.documentElement.getElementsByTagName("title").length; i++) {
-          let art: Article = {
-            'title': data.documentElement.getElementsByTagName("title")[i].innerHTML,
-            'description': data.documentElement.getElementsByTagName("description")[i].innerHTML,
-            'link': data.documentElement.getElementsByTagName("link")[i].innerHTML
+          if (data.documentElement.getElementsByTagName("description")[i]) {
+            let art: Article = {
+              'title': data.documentElement.getElementsByTagName("title")[i].innerHTML,
+              'description': data.documentElement.getElementsByTagName("description")[i].innerHTML,
+              'link': data.documentElement.getElementsByTagName("link")[i].innerHTML
+            }
+            //may want to strip broken http from this... check abeils code
+            this.articles.push(art);
+            console.log("articles updated");
           }
-          //may want to strip broken http from this... check abeils code
-          this.articles.push(art);
-          console.log("articles updated");
+
         }
         this.articleEmitter.next(this.articles)
         return this.articles;
       })
-      //need to add error handling... near here?
-      return []; //should only hit if error... because errorhandling not dealt with yet
+    //need to add error handling... near here?
+    return []; //should only hit if error... because errorhandling not dealt with yet
   }
 
 
+  //not used yet
+  getUrls(): string[] {
+    return this.urlList;
+  }
+  addUrl(url: string) {
+    this.urlList.push(url);
+  }
+  removeUrl() {
+    //not implmented...
+  }
 }
 
 // this needs to be a behavrior subject I believe -otherwise it will be hard to pas around
